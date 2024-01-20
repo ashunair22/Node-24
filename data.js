fb46f6dcf077cd50f9c6989480121bf12bc420db
ashok_nair <ashoknair035@gmail.com>
2024-01-20 12:48:48 +0530

    SecndBranchFromVideo10-Completed till video 13, crud for filesystem

---
+++ data.js
@@ -0,0 +1,5 @@
+const data = [{name: 'Ashok Nair', email: 'ashok@test.com' },
+    {name: 'Nitisha Panda', email: 'nitisha@test.com' },
+    {name: 'Ashish Nair', email: 'ashish@test.com' }
+]
+module.exports=data;
